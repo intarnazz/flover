36e7486f6153d1e8fbad62cc3953e97565d696de
Artyr Pirozhkov <velmoren@gmail.com>
2024-01-18 11:53:04 +0300

    ComponentOpener

---
+++ src/components/ComponentOpener.vue
@@ -0,0 +1,51 @@
+<script setup></script>
+
+<template>
+  <section class="opener">
+    <div class="opener__title-wrapper">
+      <div class="opener__title">Flowers, 🌻 what the world needs</div>
+      <div class="opener__sub-text">Browse between hundreds of flowers</div>
+      <button class="opener__button button">Browse</button>
+    </div>
+    <ul class="opener__img-warapper">
+      <li class="opener__img-item">
+        <img class="opener__img" src="@/assets/img/flower1.png" alt="" />
+      </li>
+      <li class="opener__img-item">
+        <img class="opener__img" src="@/assets/img/flower2.png" alt="" />
+      </li>
+      <li class="opener__img-item">
+        <img class="opener__img" src="@/assets/img/flower3.png" alt="" />
+      </li>
+      <li class="opener__img-item">
+        <img class="opener__img" src="@/assets/img/flower4.png" alt="" />
+      </li>
+      <li class="opener__img-item">
+        <img class="opener__img" src="@/assets/img/flower5.png" alt="" />
+      </li>
+      <li class="opener__img-item">
+        <img class="opener__img" src="@/assets/img/flower6.png" alt="" />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style lang="sass">
+.opener
+  display: flex
+  gap: 1em
+  &__title-wrapper
+    flex: 1
+    display: flex
+    flex-direction: column
+    gap: 2em
+    align-items: flex-start
+  &__title
+    font-size: 3.4em
+    width: 7em
+  &__img-warapper
+    flex: 1.1
+    display: flex
+    flex-wrap: wrap
+    gap: 1em
+</style>
--- src/page/PageHome.vue
+++ src/page/PageHome.vue
@@ -1,9 +1,17 @@
 <script setup>
+import ComponentOpener from "@/components/ComponentOpener.vue";
 </script>
 
 <template>
   <main class="main">
+    <ComponentOpener />
   </main>
 </template>
 
-<style lang="sass"></style>
+<style lang="sass">
+.main
+  display: flex
+  flex-direction: column
+  margin: 4em 0
+  gap: 4em
+</style>
